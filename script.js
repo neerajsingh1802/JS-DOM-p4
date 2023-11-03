86a454140202b9f4fb8b2fcff1c9eb23ccb0fcfb
neerajsingh1802 <139963133+neerajsingh1802@users.noreply.github.com>
2023-11-03 17:40:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// 1===> here we select parent element of<li> which is <ul>
+
+
+    // let elm=document.getElementById("intro");
+    // let prt=elm.parentElement;
+    // console.log(prt);
+
+//  2===> parent ke andar value set kr rhe
+
+
+    //  let elm1=document.getElementById("intro");
+    // let prt1=elm1.parentElement;
+    // prt1.innerHTML="Hello by vedanshi and bambam and tamu";
+    // console.log(prt1);
+
+// //  3===>first child select
+
+
+
+// //  3.1==>>how to select first element child 
+
+let elm2=document.getElementById("intro");
+ let node1=elm2.firstElementChild;
+console.log(node1);
+
+
+// //  3.2==>>how to select last element child 
+
+//   let elm3=document.getElementById("intro");
+//  let node2=elm3.lastElementChild;
+// console.log(node2);
+
+
+// // 3.2==>>how to select all child of element  
+
+//  let elm4=document.getElementById("intro");
+//  let node3=elm4.children;
+
+// console.log(node3);
